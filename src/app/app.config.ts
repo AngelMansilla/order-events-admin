05a932e032be38bfc7b3b221dd7624540d7f5fc0
Ángel Mansilla Puerto <angelmansillapuerto@outlook.com>
2026-08-07 18:00:58 +0200

    feat: configure Angular HTTP infrastructure

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,15 @@
+import { provideHttpClient } from '@angular/common/http';
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { API_BASE_URL } from './core/config/api.tokens';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideHttpClient(),
+    { provide: API_BASE_URL, useValue: '/api' }
+  ]
 };
